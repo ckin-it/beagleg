5bd875811ec46737bd11bde6112aeb3d5c911051
Samuele Bongiorno <bongiornosamuele@gmail.com>
2020-02-26 09:44:57 +0000

    o Adding gpios to motor-interface-constants.h

--- src/motor-interface-constants.h
+++ src/motor-interface-constants.h
@@ -94,11 +94,14 @@
 #define PIN_P8_18  (GPIO_2_BASE |  1)
 #define PIN_P8_19  (GPIO_0_BASE | 22)  // EHRPWM2A
 #define PIN_P8_26  (GPIO_1_BASE | 29)
+#define PIN_P8_27  (GPIO_2_BASE | 22)
 #define PIN_P8_28  (GPIO_2_BASE | 24)  // LCD_PCLK
+#define PIN_P8_29  (GPIO_2_BASE | 23)
 #define PIN_P8_30  (GPIO_2_BASE | 25)  // LCD_AC_BIAS_E
 #define PIN_P8_39  (GPIO_2_BASE | 12)  // LCD_DATA6
 #define PIN_P8_40  (GPIO_2_BASE | 13)  // LCD_DATA7
 #define PIN_P8_41  (GPIO_2_BASE | 10)  // LCD_DATA4
+#define PIN_P8_42  (GPIO_2_BASE | 11)
 #define PIN_P8_43  (GPIO_2_BASE | 8)   // LCD_DATA2
 #define PIN_P8_44  (GPIO_2_BASE | 9)   // LCD_DATA3
 
